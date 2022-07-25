3972c88cfc6829e538468432e18203af2aa3fe9a
David Hospital <david.hospital27@gmail.com>
2022-07-25 19:02:39 -0400

    Fix mixin entry points for WorldSaveHandler

---
+++ src/main/java/net/fabricmc/example/mixin/WorldSaveHandlerAccessor.java
@@ -0,0 +1,14 @@
+package net.fabricmc.example.mixin;
+
+import java.io.File;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import net.minecraft.world.WorldSaveHandler;
+
+@Mixin(WorldSaveHandler.class)
+public interface WorldSaveHandlerAccessor {
+    @Accessor("playerDataDir")
+    public File getPlayerDataDir();
+}
--- src/main/java/net/fabricmc/example/mixin/WorldSaveHandlerMixin.java
+++ src/main/java/net/fabricmc/example/mixin/WorldSaveHandlerMixin.java
@@ -2,28 +2,29 @@ package net.fabricmc.example.mixin;
 
 import java.io.File;
 
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.WorldSaveHandler;
 
 @Mixin(WorldSaveHandler.class)
 public class WorldSaveHandlerMixin {
     
-    @Redirect(at = @At(value = "INVOKE", target = "File(Ljava/io/File;Ljava/lang/String;)Ljava/io/File;"), 
-              method = "loadPlayerData(Lnet/minecraft/entity/player/PlayerEntity;)Lnet/minecraft/nbt/NbtCompound;")
-    private File loadPlayerDataRedirectFile(File parent, String child) {
-        System.out.println("WorldSaveHandler#loadPlayerData()#<File>()V called");
-        return new File(parent, child);
+    @Redirect(method = "loadPlayerData(Lnet/minecraft/entity/player/PlayerEntity;)Lnet/minecraft/nbt/NbtCompound;",
+              at = @At(value = "FIELD", target = "Lnet/minecraft/world/WorldSaveHandler;playerDataDir:Ljava/io/File;", opcode = Opcodes.GETFIELD))
+    private File loadPlayerDataRedirect(WorldSaveHandler worldSaveHandler) {
+        File playerDataDir = ((WorldSaveHandlerAccessor) worldSaveHandler).getPlayerDataDir();
+        System.out.println(String.format("loadPlayerDataRedirect called: %s", playerDataDir.getAbsolutePath()));
+        return playerDataDir;
     }
 
-    // @Inject(at = @At("HEAD"), method = "savePlayerData(Lnet/minecraft/entity/player/PlayerEntity;)V")
-    // private void savePlayerData(ServerPlayerEntity serverPlayerEntity, CallbackInfo info) {
-    //     System.out.println("WorldSaveHandler#savePlayerData() called");
-    // }
+    @Redirect(method = "savePlayerData(Lnet/minecraft/entity/player/PlayerEntity;)V",
+              at = @At(value = "FIELD", target = "Lnet/minecraft/world/WorldSaveHandler;playerDataDir:Ljava/io/File;", opcode = Opcodes.GETFIELD))
+    private File savePlayerDataRedirect(WorldSaveHandler worldSaveHandler) {
+        File playerDataDir = ((WorldSaveHandlerAccessor) worldSaveHandler).getPlayerDataDir();
+        System.out.println(String.format("savePlayerDataRedirect called: %s", playerDataDir.getAbsolutePath()));
+        return playerDataDir;
+    }
 }
